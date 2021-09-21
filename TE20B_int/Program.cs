54eaeaabe8f4dd27f5550144dc96ceac9f3ed80d
Mikael Bergström <krank23@gmail.com>
2021-09-21 10:52:46 +0200

    Initial commit

---
+++ TE20B_int/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+string grej = "Mina elever säger \"hej\" ";
+
+Console.WriteLine(grej);
+
+
+string name = "100";
+
+int points = 0;
+
+points = points + 1;
+
+points += 7;
+
+points++;
+
+
+points = points - 1;
+
+points -= 7;
+
+points--;
+
+
+
+int hp;
+int xp;
+
+Console.WriteLine(name + name);
+Console.WriteLine(points);
+
+Console.ReadLine();
